71f96b4ba634692c54db5a8025514c0653e84da5
anohmaelpro <56534982+anohmaelpro@users.noreply.github.com>
2020-12-10 10:00:48 +0100

    jwt

--- src/Events/JwtCreatedSubscriber.php
+++ src/Events/JwtCreatedSubscriber.php
@@ -14,7 +14,7 @@ class JwtCreatedSubscriber
           // data get on the variable $event for add new dataUser like his firstname and lastname
           $dataEventUpdate = $event->getData();
 
-          // VsCode doesn't that the $userEvent is instance of entity user that's why, those function are underlined in red color
+          // VsCode doesn't know that the $userEvent is instance of entity user that's why, those function are underlined in red color
           $dataEventUpdate['firstName'] = $userEvent->getFirstName();
           $dataEventUpdate['lastName'] = $userEvent->getLastName();
 
